7bf606e55e531f19421df2da040a2ba9035733ab
Nick Lawrence <40808955+kdggavkc@users.noreply.github.com>
2018-09-20 11:53:56 -0500

    Update display_session.py

--- display_session.py
+++ display_session.py
@@ -2,7 +2,7 @@ import shutil
 
 
 __title__ = "display-session"
-__version__ = "1.2"
+__version__ = "1.4"
 __author__ = "Nicholas Lawrence"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018-2019 Nicholas Lawrence"
